c0d7c0a595e8d315eae3e69081fe7895604b832e
Seva Kavalenka <seva.kavalenka@gmail.com>
2025-10-25 15:31:49 +0200

    feat: implement new basic js functions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create a repeating string based on the given parameters
@@ -16,11 +16,29 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let addition = "";
+
+  if ("addition" in options) {
+    const addTimes =
+      options.additionRepeatTimes !== undefined
+        ? options.additionRepeatTimes
+        : 1;
+    const addElement = String(options.addition);
+    const addSep =
+      options.additionSeparator !== undefined ? options.additionSeparator : "";
+    addition = Array(addTimes).fill(addElement).join(addSep);
+  }
+
+  const times = options.repeatTimes !== undefined ? options.repeatTimes : 1;
+  const sep = options.separator !== undefined ? options.separator : "";
+  const result = Array(times)
+    .fill(str + addition)
+    .join(sep);
+
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
